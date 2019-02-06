32af91e740091559bc81caeac235d05ecc2620de
lholdsworth <iamholdsworth@gmail.com>
2019-02-06 22:19:16 +0000

    Initial push for shard explorer

---
+++ raw/inc/footer.php
@@ -0,0 +1,23 @@
+<footer class="site-footer">
+    <div class="container">
+        
+        <div class="social">
+            <div class="joinComm">
+                <strong>Join The Community</strong>
+                <ul>
+                    <li><a href="https://t.me/devvioinc" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="https://twitter.com/devvioinc" target="_blank"><i class="fab fa-telegram-plane"></i></a></li>
+                </ul>
+            </div>
+<!--             <div class="twitterFeed">
+                <em>Twitter feed</em>
+            </div> -->
+        </div>
+
+        <div class="copyright">
+            &copy; <?php echo date('Y'); ?> Devvio Inc
+            <a href="https://www.devv.io" target="_blank">www.devv.io</a>
+        </div>
+
+    </div>  
+</footer>       
---
+++ raw/inc/header.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="./assets/img/logo.png" alt="Devvio testnet block explorer">
+        </div>
+        <a class="visitDevvio" href="https://www.devv.io" target="_blank">Visit devv.io <i class="fas fa-chevron-right"></i></a>
+    </div>
+</header>   
